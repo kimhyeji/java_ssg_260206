88718e1c9b8f6b32cd6ab2448c9bc3fe97ff85ec
kimhyeji <gpwl5589@gmail.com>
2026-02-06 14:29:34 +0900

    초기 작업

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    System.out.println("안녕");
 }
